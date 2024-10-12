453c1ecb726e7ad9ddbf382d81d03b10f9d5a236
HamzaChaudhary786 <hmzsattar99@gmail.com>
2024-10-11 23:55:11 -0700

    connected database mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import 'dotenv/config'
+import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+mongoose.connect(process.env.MONGODB_URL as string).then(() => {
+    console.log('Connected to MongoDB');
+
+})
 
 app.get('/test', async (req: Request, res: Response) => { // Removed the space here
     res.json({
